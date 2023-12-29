fc9f483d5bbde95b62404bc83f51b23f33f663fa
Ethan Obott <ethanobott6002@gmail.com>
2023-12-29 23:32:56 +0000

    changed input element's type attribute to email and modified the JS code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 //! elements from the dom
-
-// * form element
 const form = document.querySelector("#email-form");
-
-// * input 
 const inputBox = document.querySelector("#email-input");
-const inputText = document.querySelector("#email-input[type=\"text\"]");
 const errorMsg = document.querySelector("#error-msg");
 const mobileImg = document.querySelector("#mobile-image");
 const mainCard = document.querySelector("#main-card-background");
@@ -13,25 +8,26 @@ const thankYouCard = document.querySelector("#thank-you-card-background");
 const userEmailInsert = document.querySelector("#email-insert");
 
 
-//* form event listener
+//! submit event listener for the form that processes the user input when the form is submitted
 form.addEventListener("submit", (event) => {
     event.preventDefault(); //? prevents the page from being refreshed and the form sending any data to the backend
 
-    //* user email input
+    //* stores the input that the user types into the form
     const userEmailInput = document.querySelector("#email-input").value;
 
-    //* validator.js to validate email
+    //* validator.js library to validate email input
     const validation = validator.isEmail(userEmailInput);
+
 
     // * function that does some styling when the user types in the incorrect email 
     emailIncorrect = () => {
         inputBox.style.backgroundColor = "var(--tomato-20-opaque)";
         inputBox.style.border = "1px solid #ff000085";
-        inputText.style.color = "var(--tomato-70-opaque)";
+        inputBox.style.color = "var(--tomato-70-opaque)";
         errorMsg.style.display = "block";
     }
 
-    // * function that does some styling when the user types in the correct email 
+    // // * function that does some styling when the user types in the correct email 
     emailCorrect = () => {
         mobileImg.style.display = "none";
         mainCard.style.display = "none";
@@ -39,7 +35,7 @@ form.addEventListener("submit", (event) => {
         userEmailInsert.innerText = userEmailInput;
     }
 
-    //* conditional statement to run the functions
+    //* conditional statement to run the either of the functions  based on whether the email input is correct or not
     !validation ? emailIncorrect() : emailCorrect();
 })
 
