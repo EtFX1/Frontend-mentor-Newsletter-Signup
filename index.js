2faf472295db7b3e961f342f92d5a426ab60dca1
Ethan Obott <ethanobott6002@gmail.com>
2024-01-24 10:17:38 +0000

    fixed the mobile thank you page

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 //! elements from the dom
-const form = document.querySelector("#email-form");
-const inputBox = document.querySelector("#email-input");
-const errorMsg = document.querySelector("#error-msg");
-const mobileImg = document.querySelector("#mobile-image");
-const mainCard = document.querySelector("#main-card-background");
-const thankYouCard = document.querySelector("#thank-you-card-background");
-const userEmailInsert = document.querySelector("#email-insert");
+const form = document.querySelector(".email-form");
+const inputBox = document.querySelector(".email-input");
+const mainCard = document.querySelector(".main-card-background");
+const thankYouCard = document.querySelector(".thank-you-card-background");
+const userEmailInsert = document.querySelector(".email-insert");
 
 
 //! submit event listener for the form that processes the user input when the form is submitted
@@ -13,30 +11,39 @@ form.addEventListener("submit", (event) => {
     event.preventDefault(); //? prevents the page from being refreshed and the form sending any data to the backend
 
     //* stores the input that the user types into the form
-    const userEmailInput = document.querySelector("#email-input").value;
+    const userEmailInput = document.querySelector(".email-input").value;
+    console.log(userEmailInput);
 
     //* validator.js library to validate email input
     const validation = validator.isEmail(userEmailInput);
 
 
-    // * function that does some styling when the user types in the incorrect email 
+    // * function that runs when the email entered is incorrect
     emailIncorrect = () => {
-        inputBox.style.backgroundColor = "var(--tomato-20-opaque)";
-        inputBox.style.border = "1px solid #ff000085";
-        inputBox.style.color = "var(--tomato-70-opaque)";
-        errorMsg.style.display = "block";
+        //* the input textbox turns red
+        inputBox.classList.add("email-input-error");
+
+        //* the error message is displayed
+        errorMsg.classList.add("displayed-error-msg");
     }
 
-    // // * function that does some styling when the user types in the correct email 
+    // emailIncorrect();
+
+    // * function that does some styling when the user types in the correct email 
     emailCorrect = () => {
-        mobileImg.style.display = "none";
-        mainCard.style.display = "none";
-        thankYouCard.style.display = "block";
+
+        // * the main card dissapears
+        mainCard.classList.add("display-none");
+
+        //* the thank you card appears
+        thankYouCard.classList.add("display-block");
         userEmailInsert.innerText = userEmailInput;
     }
 
-    //* conditional statement to run the either of the functions  based on whether the email input is correct or not
-    !validation ? emailIncorrect() : emailCorrect();
+    emailCorrect()
+
+    // //* conditional statement to run the either of the functions  based on whether the email input is correct or not
+    // !validation ? emailIncorrect() : emailCorrect();
 })
 
 
